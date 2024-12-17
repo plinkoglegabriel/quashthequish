c98e329cf2fdc43b6e121597b88cbf7e3b11c4c5
Polly L-G <plinkoglegabriel@gmail.com>
2024-12-17 09:35:53 +0000

    Adding comments to app.py file

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
+# Importing the necessary libraries and modules including the Flask class
 from flask import Flask, request, jsonify
+# Importing CORS to allow cross-origin requests
 from flask_cors import CORS
+# Importing the qrCodeAnalyser funtion from the detectionAlgorithm.py file
 from detectionAlgorithm import qrCodeAnalyser
 
+# Creating and naming the instance of the Flask class
 app = Flask(__name__)
 
+# Passing the app instance to CORS
 CORS(app)
+# Defining the URL to trigger the validate function via a POST request
 @app.route('/validate', methods=['POST'])
 
 def validate():
