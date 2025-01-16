67d7445c138987030b4130bdfb53520c97304772
Polly L-G <plinkoglegabriel@gmail.com>
2025-01-16 09:21:47 +0000

    adding CameraScan.tsx component template and adding expo camera plugin to app.json

---
+++ frontend/app/CameraScan.tsx
@@ -0,0 +1,17 @@
+// import react and other necessary modules from react-native
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+// import necessary modules from expo-camera
+import { CameraView, useCameraPermissions, CameraType } from "expo-camera";
+
+// CameraScan component
+export default function CameraScan() {
+  return <View />;
+}
