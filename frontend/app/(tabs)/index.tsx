63a838a7b87442197bf9c195741e80169d6901e2
Polly L-G <plinkoglegabriel@gmail.com>
2025-01-25 09:47:24 +0000

    Updating index.tsx to accomodate CameraScan

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+// Import necessary modules
 import React from "react";
 import {
   View,
@@ -9,35 +10,56 @@ import {
 import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "../../styles";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../types";
 
+// Define navigation prop's type
+type NavigationProp = StackNavigationProp<RootStackParamList>;
+
+// HomeScreen component
 export default function HomeScreen() {
+  // Load custom font for the app using useFonts hook
   const [fontsLoaded] = useFonts({
     Michroma: require("../../assets/fonts/Michroma-Regular.ttf"),
   });
+  // Getting navigation prop from useNavigation hook
+  const navigation = useNavigation<NavigationProp>();
 
+  // If fonts are not loaded, show an loading indicator
   if (!fontsLoaded) {
     return <ActivityIndicator size="large" color="#ffffff" />;
   }
 
   return (
+    // Home screen render
     <View style={styles.container}>
+      {/* Displaying user's usename (template for now) */}
       <Text style={[styles.heading2, { fontFamily: "Michroma" }]}>
         Hello ______!
       </Text>
+      {/* Displaying logo */}
       <Image
         source={require("../../assets/images/quash-the-quish-logo.png")}
         style={styles.image}
       />
+      {/* Displaying button to scan */}
       <Text style={[styles.heading2, { fontFamily: "Michroma" }]}>
         Scan or Upload to Test your QR Code
       </Text>
       <View style={styles.actionsContainer}>
-        <TouchableOpacity style={styles.actionButton}>
+        {/* Handling logic to CameraScan */}
+        <TouchableOpacity
+          style={styles.actionButton}
+          onPress={() => navigation.navigate("CameraScan")}
+        >
+          {/* Camera Icon */}
           <Ionicons name="camera-outline" size={40} color="white" />
           <Text style={[styles.buttonText, { fontFamily: "Michroma" }]}>
             Camera
           </Text>
         </TouchableOpacity>
+        {/* Handling logic to UploadScreen (template for now) */}
         <TouchableOpacity style={styles.actionButton}>
           <Ionicons name="cloud-upload-outline" size={40} color="white" />
           <Text style={[styles.buttonText, { fontFamily: "Michroma" }]}>
